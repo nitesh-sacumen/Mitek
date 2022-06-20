3cb854f184af72892f928c0bb468d5f7520cd6fe
rahul <rahul.parmar@sacumen.com>
2022-06-20 14:32:50 +0530

    added code for capture

--- src/main/java/com/mitek/tree/nodes/Capture.java
+++ src/main/java/com/mitek/tree/nodes/Capture.java
@@ -1,26 +1,28 @@
 package com.mitek.tree.nodes;
 
-import com.google.common.collect.ImmutableList;
 import com.mitek.tree.config.Constants;
 import com.sun.identity.authentication.callbacks.HiddenValueCallback;
 import com.sun.identity.authentication.callbacks.ScriptTextOutputCallback;
 import com.sun.identity.authentication.client.AuthClientUtils;
 import org.forgerock.json.JsonValue;
-import org.forgerock.openam.auth.node.api.*;
+import org.forgerock.openam.auth.node.api.Action;
+import org.forgerock.openam.auth.node.api.Node;
+import org.forgerock.openam.auth.node.api.SingleOutcomeNode;
+import org.forgerock.openam.auth.node.api.TreeContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.TextOutputCallback;
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.forgerock.openam.auth.node.api.Action.send;
 
 @Node.Metadata(outcomeProvider = SingleOutcomeNode.OutcomeProvider.class, configClass = Capture.Config.class)
 public class Capture extends SingleOutcomeNode {
 
-    private static Logger utilDebug = LoggerFactory.getLogger(AuthClientUtils.class);
+
+    private static Logger logger = LoggerFactory.getLogger(AuthClientUtils.class);
 
     /**
      * Configuration for the node.
@@ -31,42 +33,58 @@ public class Capture extends SingleOutcomeNode {
 
     @Inject
     public Capture() {
-    }
 
-
-    private Action buildCallbacks(TreeContext context) {
-        NodeState sharedState = context.getStateFor(this);
-        String verificationChoice = sharedState.get(Constants.VERIFICATION_CHOICE).asString();
-        return send(new ArrayList<Callback>() {{
-            add(new ScriptTextOutputCallback(getAuthDataScript("/mitek/p1.js", verificationChoice)));
-            add(new HiddenValueCallback("imageURL"));
-        }}).build();
     }
 
     @Override
     public Action process(TreeContext context) {
+        logger.debug("*********************Capture node********************");
         JsonValue sharedState = context.sharedState;
-        if (context.hasCallbacks()) {
-            String imageData = context.getCallback(HiddenValueCallback.class).get().getValue();
-            utilDebug.debug("*********imageData**********" + imageData);
-            sharedState.add("imageRef",imageData);
-            return goToNext().replaceSharedState(sharedState).build();
+        Boolean isCaptureRefresh = false;
+        if (sharedState.get(Constants.IS_CAPTURE_REFRESH).isNotNull()) {
+            isCaptureRefresh = sharedState.get(Constants.IS_CAPTURE_REFRESH).asBoolean();
+            if (isCaptureRefresh == true) {
+                sharedState.put(Constants.IS_CAPTURE_REFRESH, false);
+            }
         }
 
-        return buildCallbacks(context);
+        String verificationChoice = sharedState.get(Constants.VERIFICATION_CHOICE).asString();
+        String url = "/mitek/p1.js";
+        System.out.println("into capture");
+        if (!context.getCallback(HiddenValueCallback.class).isEmpty()) {
+            System.out.println("into capture has callback");
+            String imageData = context.getCallback(HiddenValueCallback.class).get().getValue();
+            logger.debug("*********imageData**********" + imageData);
+            sharedState.put("captureResult", imageData);
+            return goToNext().replaceSharedState(sharedState).build();
+        }
+        System.out.println("into capture no callback");
+        System.out.println("verification is:");
+        return buildCallbacks(url, verificationChoice, isCaptureRefresh);
+    }
+
+    private Action buildCallbacks(String url, String verificationChoice, Boolean isCaptureRefresh) {
+        return send(new ArrayList<>() {{
+            add(new TextOutputCallback(0, "Please wait after image capture, it will be displayed shortly for preview."));
+            add(new ScriptTextOutputCallback(getAuthDataScript(url, verificationChoice, isCaptureRefresh)));
+            add(new HiddenValueCallback("captureResponse"));
+        }}).build();
 
     }
 
-    private String getAuthDataScript(String scriptURL, String verificationChoice) {
+    private String getAuthDataScript(String scriptURL, String verificationChoice, Boolean isCaptureRefresh) {
         return "var loadJS = function(url, implementationCode, location){\r\n" +
                 "    var scriptTag = document.createElement('script');\r\n" +
+                "scriptTag.id='mitekScript';\n" +
                 "    scriptTag.src = url;\r\n" +
                 " var link = document.createElement('link');\r\n" +
                 "link.rel = 'stylesheet';\r\n" +
                 "link.type = 'text/css';\r\n" +
                 "link.href = '/mitek/style.css';\r\n" +
+                "document.getElementById('loginButton_0').style.display = 'none';\n" +
                 "scriptTag.appendChild(link);\r\n" +
                 "location.appendChild(scriptTag);\r\n" + "};\r\n" +
+
                 "var input = document.createElement('input');\r\n" + "input.setAttribute('type', 'hidden');\r\n" +
 
                 "input.setAttribute('id', 'integratorDocTypeInput');\r\n" +
@@ -74,18 +92,15 @@ public class Capture extends SingleOutcomeNode {
                 "input.setAttribute('value','" + verificationChoice + "');\r\n" +
 
                 "document.body.appendChild(input);\r\n" +
+
+
                 "setTimeout(function(){\n" +
                 "var imageData = document.getElementById('capturedImage').src;\n" +
-                "console.log('-----------------image',imageData);\n" +
-                "document.getElementById('imageURL').value=imageData;\n" +
-                "}, 25000);\r\n" +
+                "document.getElementById('captureResponse').value=imageData;\n" +
+                "document.getElementById('loginButton_0').click();" +
+                "}, 15000);\r\n" +
                 "var yourCodeToBeCalled = function(){\r\n" +
-                "var imageData = document.getElementById('capturedImage').src;\n" +
-                "console.log('-----------------image',imageData);\n" +
-                "document.getElementById('imageURL').value=imageData;\n" +
                 "}\r\n" + "loadJS(" + "\"" + scriptURL + "\"" + ", yourCodeToBeCalled, document.body);";
 
     }
-
-
 }
